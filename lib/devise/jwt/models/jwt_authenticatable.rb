0a0b8657783eef74f184dcc63c26b0a1638078c7
Tzachi (Raphael) Fraiman <tzachif@folloze.com>
2020-05-27 14:08:53 -0400

    Update jwt_authenticatable.rb

--- lib/devise/jwt/models/jwt_authenticatable.rb
+++ lib/devise/jwt/models/jwt_authenticatable.rb
@@ -11,16 +11,10 @@ module Devise
     module JwtAuthenticatable
       extend ActiveSupport::Concern
       
-      module ClassMethods
+      included do
         def self.find_for_jwt_authentication(sub)
           find_by(primary_key => sub)
         end
-        
-        Devise::Models.config(self, :jwt_revocation_strategy)
-      end
-
-      def self.included(base)
-        base.extend(ClassMethods)
       end
 
       def jwt_subject
